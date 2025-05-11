cb4a708d61d36a8388a0cadffee1b1966d5cfe82
Akshay Panchivala <akshaypanchivala@gmail.com>
2025-05-11 16:48:36 +0530

    fixed  prod issue

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,4 @@
+export const baseUrl = import.meta.env.VITE_API_BASE_URL;
 export const initialMessage=`**Hello!** Welcome to **File Talk AI**, your intelligent document assistant.
 How can I assist you in organizing, analyzing, or processing your documents today? Just ask away, and let’s make document management easier together!
 ### Please select one of the options below to begin your journey with  **File Talk AI**`
@@ -13,5 +14,3 @@ I'm getting ready to create thoughtful questions based on your content.
 
 export const questionAnswerMessage=`You've selected the **Question & Answer** option! 💬  
 Just type your question in the input box, and I'll help you find the best answer. `
-
-export const baseUrl="http://127.0.0.1:8000/api/v1/chatbot/"
